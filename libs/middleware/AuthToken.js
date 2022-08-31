893a9b7d24ea6b05a0829c1aeb07097081ff3d17
kleynervillegas <kleynervillegas@gmail.com>
2022-08-31 16:30:32 -0400

    actualizando

--- libs/middleware/AuthToken.js
+++ libs/middleware/AuthToken.js
@@ -1,4 +1,4 @@
-import jwt from 'jsonwebtoken';
+import jwt from'jsonwebtoken';
 import { MESSAGES, STATUS } from '../common/utils/Constants.js';
 import { ResponseService } from '../common/utils/ResponseService.js';
 
@@ -11,13 +11,8 @@ const response = new ResponseService(
 export default function (req, res, next) {
   if (req.headers.token) {
     jwt.verify(req.headers.token, process.env['jwtSecret'], function (err, decoded) {
-      if (err) {
-        if (err.message === 'jwt expired') {
-          response.message = MESSAGES.sesionExpire
-          res.status(STATUS.Unauthorized).send(response);
-        } else {
-          res.status(STATUS.Unauthorized).send(response);
-        }
+      if (err) {        
+        res.status(STATUS.Unauthorized).send(response);
       } else {
         req.user = decoded.user;
         next()
